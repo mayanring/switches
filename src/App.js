897ccd4e0252d1c6c06f0a87f1d8605a288344d4
Ryan Ming <ryan.ming@gmail.com>
2018-02-15 23:32:12 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react"
+import logo from "./logo.svg"
+import "normalize.css"
+import "./App.css"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="switch-container">
+        <div className="switch">yo</div>
+      </div>
+    )
+  }
+}
+
+export default App
