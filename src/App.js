651b46ddf417af9d7717ba3970cba3fb2ff1079f
Ryan Ming <ryan.ming@gmail.com>
2018-02-16 00:03:23 -0500

    added switches and stylin

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React, { Component } from "react"
-import logo from "./logo.svg"
+import Switch from "react-ios-switch"
 import "normalize.css"
 import "./App.css"
 
+const SWITCH_COUNT = 25
+
 class App extends Component {
+  state = { switches: Array(SWITCH_COUNT).fill(false) }
+
+  handleSwitched = (checked, i) => {
+    const switches = this.state.switches
+    switches[i] = checked
+
+    this.setState({
+      switches: switches
+    })
+  }
+
   render() {
+    const switches = this.state.switches
+
     return (
       <div className="switch-container">
-        <div className="switch">yo</div>
+        {switches.map((checked, i) => (
+          <div className="col" key={i}>
+            <Switch
+              checked={checked}
+              onChange={checked => this.handleSwitched(checked, i)}
+              className="switch"
+            />
+          </div>
+        ))}
       </div>
     )
   }
